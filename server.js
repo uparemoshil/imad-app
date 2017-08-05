8f12a38e6dfb6d3b59cfa4f320bfc5106615124e
uparemoshil <30691234+uparemoshil@users.noreply.github.com>
2017-08-05 15:26:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artical-one',function(req,res) {
+app.get('/artical-one',function(req, res) {
     res.send('artical one is requested and served here');
 });
 
-app.get('/artical-two',function(req,res) {
+app.get('/artical-two',function(req, res) {
     res.send('artical two is requested and served here');
 });
-    app.get('/artical-three',function(req,res) {
+    app.get('/artical-three',function(req, res) {
     res.send('artical three is requested and served here');
     });
 app.get('/ui/style.css', function (req, res) {
