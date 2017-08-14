0b774f88419c242f16d8c3d74c0ac87b54637884
uparemoshil <30691234+uparemoshil@users.noreply.github.com>
2017-08-14 14:48:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,20 @@ button.onclick=function(){
     var span=document.getElementById("count");
     span.innerHTML= counter.toString();
 };
+ 
+ var nameinput=document.getElementById("name");
+ var name = nameinput.value;
+ var submit=document.getElementById("submit_btn");
+  submit.onclick= function(){
+      // capture a list of names and render it.
+      var names=['name1', 'name2', 'name3','name4'];
+      var list='';
+      for(var i=0; i<name.lenght,i++)
+      {
+          list += "<li>" + names[i] + "</li>" ;
+          
+      }
+      var ul=document.getElementById("namelist");
+      ul.innerHTML= list;
+  };
+ 
