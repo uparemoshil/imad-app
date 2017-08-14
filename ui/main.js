881fb72f02cdf286702042787da6628753676f49
uparemoshil <30691234+uparemoshil@users.noreply.github.com>
2017-08-14 14:11:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,5 @@ var button=document.getElementById("counter");
 button.onclick=function(){
     counter=counter +1;
     var span=document.getElementById("count");
-    span.innerHTML=counter.tostring();
+    span.innerHTML= counter.toString();
 };
